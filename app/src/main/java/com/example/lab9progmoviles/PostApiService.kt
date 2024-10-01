f4dbb760834236e55567e10a1fcf22df5039c5fd
WisomdCV <wilsondcv711@gmail.com>
2024-10-01 15:54:52 -0500

    InitialConfig
    Configuración que abarca:
    
    - Permisos
    - PostApiService
    - PostModel

---
+++ app/src/main/java/com/example/lab9progmoviles/PostApiService.kt
@@ -0,0 +1,13 @@
+package com.example.lab9progmoviles
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PostApiService {
+    @GET("posts")
+    suspend fun getUserPosts(): List<PostModel>
+
+    @GET("posts/{id}")
+    suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
+}
